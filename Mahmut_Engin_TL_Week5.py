abdec8525f513921ac4c8df9b13e18246c943617
MahmutEngin <155198520+MahmutEngin@users.noreply.github.com>
2025-05-17 01:01:53 +0200

    Update Mahmut_Engin_TL_Week5.py

--- Mahmut_Engin_TL_Week5.py
+++ Mahmut_Engin_TL_Week5.py
@@ -57,3 +57,70 @@ class Square(Shape):
         super().__init__(width,width)
     def area_calculate(self):
         return self.width*self.width
+rectangle =Rectangle(4,5)
+sequare = Square(5)
+print("Rectangle Area: ", rectangle.calculate_area())
+print("Square Area : ", sequare.area_calculate())
+
+# Q4
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make=make
+        self.model =model
+        self.year =year
+class SuvVehicle(Vehicle):
+    def __init__(self, make, model, year,four_wheel):
+        super().__init__(make, model, year)
+        self.four_wheel =four_wheel
+
+class SportsCar(Vehicle):
+    def __init__(self, make, model, year, max_speed):
+        super().__init__(make, model, year)
+        self.max_speed=max_speed
+suv_vhicle= SuvVehicle("Toyota","Rav4",2025,True)
+sport_vhicle=SportsCar("BMW",328, 2025, 250)
+
+print(f"Off-Road Vhicle: Make: {suv_vhicle.make},Model: {suv_vhicle.model}, Year : {suv_vhicle.year}, Four Wheel Drive:  {suv_vhicle.four_wheel} ")
+
+print(f"Sport Vhicle: Make: {sport_vhicle.make},Model: {sport_vhicle.model}, Year : {sport_vhicle.year}, Four Wheel Drive:  {sport_vhicle.max_speed} ")
+
+
+# Q5
+
+class Customer:
+    def __init__(self,name,surname,tc_identification,phone):
+        self.name=name
+        self.surname=surname
+        self.tc_identification=tc_identification
+        self.phone=phone
+ 
+    def musteri_bilgileri (self):
+        return f"Name: {self.name},  Surname: {self.surname},  TR ID Number:  {self.tc_identification}, Telephone Number: {self.phone}"
+class Account(Customer):
+    def __init__(self, name, surname, tc_identification, phone,customer,account_number):
+        super().__init__(name, surname, tc_identification, phone)
+        self.customer=customer
+        self.account_number=account_number
+        self.balance=0.0
+    def deposit(self, amount):
+        self.balance+=amount
+        print(f"Depozit: {amount}, Bakiye : {self.balance}")
+    def money_check(self, amount):
+        if amount <=self.balance:
+            self.balance -=amount
+            print(f"Cekilen: {amount}. Yeni bakiye: {self.balance}")
+        else:
+            print("Yeterli bakiye yok")
+
+    def display_balance(self):
+        print(f" Mevcut bakiye:{self.balance} ")
+
+
+account = Account("Ali", "Veli", "12345678901", "05551234567", "TR123456")
+print(account.musteri_bilgileri())
+
+account.deposit(1000)
+account.display_balance()
+account.money_check(300)
+account.display_balance()
+account.money_check(800)
